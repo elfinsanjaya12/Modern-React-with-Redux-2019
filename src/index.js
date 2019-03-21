3631a833c45e4011409252f0e0be00f41163b3d3
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2019-03-21 12:12:12 +0700

    Building content with JSX

--- src/index.js
+++ src/index.js
@@ -3,9 +3,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// function getButtonText() {
+//   return "Click on me!";
+// }
 // Creat a react component
 const App = () => {
-  return <div>Hi there!</div>;
+  const buttonText = { text: "Click Me" };
+  const labelText = "Enter name: ";
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
 };
 
 // Take the rect component and show it on the screan
